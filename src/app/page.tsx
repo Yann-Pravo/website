1a7976061022d64e31641b4ee520df1d43b76135
Yann Pravo <yann.pravo@gmail.com>
2025-03-03 14:30:54 +0100

    add show more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,165 +1,179 @@
 'use client'
 
 import classNames from "classnames";
-import React from "react";
-import { JOBS } from "../data/data";
-import ProfileDesc from "../data/profile.mdx"
+import React, { useState } from "react";
+import { JOBS, PROFILE } from "../data/data";
 import { AcademicCapIcon, AtSymbolIcon, BriefcaseIcon, EnvelopeIcon, MapPinIcon, UserIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
 
 const Home = () => {
+  const [showMore, setShowMore] = useState(false)
+
+  const onShowMore = () => {
+    setShowMore(!showMore)
+  }
+
   return (
-    <div id="printable-content" className="bg-white lg:shadow-lg p-8 rounded-lg lg:max-w-5xl mx-auto text-gray-800 font-sans">
-      {/* Header */}
-      <header className="text-center pb-6">
-        <h1 className="uppercase text-2xl font-bold">Yann Pravossoudovitch</h1>
-        <p className="mt-2 flex items-center justify-center">
-          FULL STACK WEB DEVELOPER
-        </p>
-      </header>
+    <>
+      <div id="printable-content" className="bg-white lg:shadow-lg px-8 py-20 rounded-lg lg:max-w-5xl mx-auto text-gray-800 font-sans">
+        {/* Header */}
+        <header className="text-center pb-6">
+          <h1 className="uppercase text-2xl font-bold">Yann Pravossoudovitch</h1>
+          <p className="mt-2 flex items-center justify-center">
+            FULL STACK WEB DEVELOPER
+          </p>
+        </header>
 
-      <div className="mt-6 flex flex-col lg:flex-row gap-8">
-        {/* Sidebar */}
-        <aside className="flex justify-center space-x-6 sm:space-x-12 lg:space-x-0 lg:block lg:space-y-8 px-4">
-          {/* Details */}
-          <section className="flex flex-col items-center">
-            <h2 className="font-semibold mb-2">◦ DETAILS ◦</h2>
-            <p className="flex items-center mt-1">
-            <EnvelopeIcon className="mr-2 size-4" /> yann.pravo<AtSymbolIcon className="size-4" />gmail.com
-            </p>
-            <p className="flex items-center"><MapPinIcon className="ml-4 mr-0.5 size-4" />France</p>
-          </section>
+        <div className="mt-6 flex flex-col lg:flex-row gap-8">
+          {/* Sidebar */}
+          <aside className="flex justify-center space-x-6 sm:space-x-12 lg:space-x-0 lg:block lg:space-y-8 px-4">
+            {/* Details */}
+            <section className="flex flex-col items-center">
+              <h2 className="font-semibold mb-2">◦ DETAILS ◦</h2>
+              <p className="flex items-center mt-1">
+              <EnvelopeIcon className="mr-2 size-4" /> yann.pravo<AtSymbolIcon className="size-4" />gmail.com
+              </p>
+              <p className="flex items-center"><MapPinIcon className="ml-4 mr-0.5 size-4" />France</p>
+            </section>
 
-          {/* Links */}
-          <section className="flex flex-col items-center">
-            <h2 className="font-semibold mb-2">◦ LINKS ◦</h2>
-            <Link
-              href="https://www.linkedin.com/in/yann-pravossoudovitch-55883881"
-              className="flex items-center underline mt-1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </Link>
-            <Link
-              href="https://github.com/Yann-Pravo"
-              className="flex items-center underline mt-1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </Link>
-          </section>
-
-          {/* Skills */}
-          <section className="hidden lg:flex flex-col items-center">
-            <h2 className="font-semibold mb-2">◦ SKILLS ◦</h2>
-            <ul className="space-y-1 mt-1 list-none flex flex-col items-center">
-              {[
-                "Continuous deployment",
-                "Relational databases",
-                "Data visualization",
-                "User Experience",
-                "Communication",
-                "REST APIs",
-                "Git",
-              ].map((skill) => (
-                <li key={skill}>{skill}</li>
-              ))}
-            </ul>
-          </section>
-        </aside>
-
-        {/* Main Content */}
-        <main className="lg:flex-grow space-y-8">
-          {/* Profile */}
-          <section>
-            <h2 className="font-semibold mb-2 flex items-center"><UserIcon className="mr-2 size-4" />PROFILE</h2>
-            <div className="relative flex gap-x-4">
-              <div
-                className="absolute left-0 top-0 flex w-6 bottom-1 justify-center"
+            {/* Links */}
+            <section className="flex flex-col items-center">
+              <h2 className="font-semibold mb-2">◦ LINKS ◦</h2>
+              <Link
+                href="https://www.linkedin.com/in/yann-pravossoudovitch-55883881"
+                className="flex items-center underline mt-1"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <div className="w-px bg-gray-200" />
-              </div>
-              <div className="relative flex size-6 flex-none items-center justify-center bg-white">
-                <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-              </div>
-              <div className="space-y-2">
-                <ProfileDesc />
-              </div>
-            </div>
-          </section>
+                LinkedIn
+              </Link>
+              <Link
+                href="https://github.com/Yann-Pravo"
+                className="flex items-center underline mt-1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </Link>
+            </section>
 
-          {/* Employment History with Vertical Timeline */}
-          <section>
-            <h2 className="font-semibold mb-2 flex items-center"><BriefcaseIcon className="mr-2 size-4" />EMPLOYMENT HISTORY</h2>
-            <ul
-              role="list"
-              className="space-y-8"
-            >
-              {JOBS.map(({name, role, website, location, type, date, description: Description}, index) => (
-                <li key={index} className="relative flex gap-x-4">
-                  <div
-                    className={classNames(
-                      index === JOBS.length - 1 ? 'h-6' : '-bottom-6',
-                      'absolute left-0 top-0 flex w-6 justify-center',
-                    )}
-                  >
-                    <div className="w-px bg-gray-200" />
-                  </div>
-                  <div className="relative flex size-6 flex-none items-center justify-center bg-white">
-                    <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-                  </div>
-                  <div>
-                    <div className="sm:flex items-center">
-                      <div className="space-x-1">
-                        <h3 className="font-semibold inline">{role}</h3>
-                          <Link
-                            href={website}
-                            className="inline underline"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {name}
-                          </Link>
+            {/* Skills */}
+            <section className="hidden lg:flex flex-col items-center">
+              <h2 className="font-semibold mb-2">◦ SKILLS ◦</h2>
+              <ul className="space-y-1 mt-1 list-none flex flex-col items-center">
+                {[
+                  "Continuous deployment",
+                  "Relational databases",
+                  "Data visualization",
+                  "User Experience",
+                  "Communication",
+                  "REST APIs",
+                  "Git",
+                ].map((skill) => (
+                  <li key={skill}>{skill}</li>
+                ))}
+              </ul>
+            </section>
+          </aside>
+
+          {/* Main Content */}
+          <main className="lg:flex-grow space-y-8">
+            {/* Profile */}
+            <section>
+              <h2 className="font-semibold mb-2 flex items-center"><UserIcon className="mr-2 size-4" />PROFILE</h2>
+              <div className="relative flex gap-x-4">
+                <div
+                  className="absolute left-0 top-0 flex w-6 bottom-1 justify-center"
+                >
+                  <div className="border-l border--gray-200" />
+                </div>
+                <div className="relative flex size-6 flex-none items-center justify-center bg-white">
+                  <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                </div>
+                <div className="space-y-2">
+                  {showMore ? <PROFILE.description /> : PROFILE.short}
+                </div>
+              </div>
+            </section>
+
+            {/* Employment History with Vertical Timeline */}
+            <section>
+              <h2 className="font-semibold mb-2 flex items-center"><BriefcaseIcon className="mr-2 size-4" />EMPLOYMENT HISTORY</h2>
+              <ul
+                role="list"
+                className="space-y-4"
+              >
+                {JOBS.map(({name, role, website, location, type, date, short, description: Description}, index) => (
+                  <li key={index} className="relative flex gap-x-4">
+                    <div
+                      className={classNames(
+                        'absolute left-0 top-0 flex w-6 justify-center',
+                        {
+                          '-bottom-6': index !== JOBS.length - 1,
+                          'h-12': index === JOBS.length - 1 && !showMore,
+                          'h-24': index === JOBS.length - 1 && showMore,
+                        }
+                      )}
+                    >
+                      <div className="border-l border-gray-200" />
+                    </div>
+                    <div className="relative flex size-6 flex-none items-center justify-center bg-white">
+                      <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                    </div>
+                    <div>
+                      <div className="sm:flex items-center">
+                        <div className="space-x-1">
+                          <h3 className="font-semibold inline">{role}</h3>
+                            <Link
+                              href={website}
+                              className="inline underline"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {name}
+                            </Link>
+                        </div>
+                        <div className="flex items-center sm:ml-2 space-x-1 text-gray-600 text-sm">
+                          {type && <p>{type}, </p>}
+                          <p>{location}, </p>
+                          <p>{date}</p>
+                        </div>
                       </div>
-                      <div className="flex items-center sm:ml-2 space-x-1 text-gray-600 text-sm">
-                        {type && <p>{type}, </p>}
-                        <p>{location}, </p>
-                        <p>{date}</p>
+                      <div className="mt-1 space-y-2 desc">
+                        {showMore ? <Description /> : short}
                       </div>
                     </div>
-                    <div className="mt-1 space-y-2 desc"><Description /></div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </section>
+                  </li>
+                ))}
+              </ul>
+            </section>
 
-          {/* Education */}
-          <section>
-            <h2 className="font-semibold mb-2 flex items-center"><AcademicCapIcon className="mr-2 size-4" />EDUCATION</h2>
-            <div className="relative flex gap-x-4">
-              <div
-                className="absolute left-0 top-0 flex w-6 bottom-1 justify-center"
-              >
-                <div className="w-px bg-gray-200" />
+            {/* Education */}
+            <section>
+              <h2 className="font-semibold mb-2 flex items-center"><AcademicCapIcon className="mr-2 size-4" />EDUCATION</h2>
+              <div className="relative flex gap-x-4">
+                <div
+                  className="absolute left-0 top-0 flex w-6 bottom-1 justify-center"
+                >
+                  <div className="border-l border-gray-200" />
+                </div>
+                <div className="relative flex size-6 flex-none items-center justify-center bg-white">
+                  <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                </div>
+                <div>
+                  <p className="font-semibold">
+                    Master of Science, Ecole polytechnique universitaire de
+                    Montpellier, France
+                  </p>
+                  <p className="text-gray-600 text-sm">2009 - 2013</p>
+                </div>
               </div>
-              <div className="relative flex size-6 flex-none items-center justify-center bg-white">
-                <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-              </div>
-              <div>
-                <p className="font-semibold">
-                  Master of Science, Ecole polytechnique universitaire de
-                  Montpellier, France
-                </p>
-                <p className="text-gray-600 text-sm">2009 - 2013</p>
-              </div>
-            </div>
-          </section>
-        </main>
+            </section>
+          </main>
+        </div>
       </div>
-    </div>
+      <button className="bg-white lg:bg-transparent p-2 rounded-xl fixed right-4 bottom-4 underline" onClick={onShowMore}>Show {showMore ? "less" : "more"}</button>
+    </>
   );
 };
 
--- src/data/data.ts
+++ src/data/data.ts
@@ -5,33 +5,37 @@ import RapsodieDesc from './jobs/rapsodie.mdx'
 import MassDesc from './jobs/mass.mdx'
 import XStudioDesc from './jobs/xstudio.mdx'
 import MemoriselyDesc from './jobs/memorisely.mdx'
+import ProfileDesc from "./profile.mdx"
 
 export const JOBS = [
   {
     name: "Memorisely",
-    role: "Freelance, Senior developer at",
+    role: "Senior developer at",
     location: "United-Kingdom",
     type: "Freelance",
     date: "2025",
     description: MemoriselyDesc,
+    short: "Built critical features as Job Board using Next.js, TypeScript, Stripe, forms and validation.",
     website: "https://www.memorisely.com/"
   },
   {
     name: "X-Studio",
-    role: "Freelance, Fullstack developer at",
+    role: "Fullstack developer at",
     location: "France",
     type: "Freelance",
     date: "2024",
     description: XStudioDesc,
+    short: "Built scalable, efficient web applications using Next.js, TypeScript, and Tailwind.",
     website: "https://www.x-studio.fr/"
   },
   {
     name: "Mass",
-    role: "Freelance, Senior Frontend developer at",
+    role: "Senior Frontend developer at",
     location: "France",
     type: "Freelance",
     date: "2023",
     description: MassDesc,
+    short: "Developed functionalities such as buy, sell, and bridge between cryptocurrencies.",
     website: "https://mass.money/"
   },
   {
@@ -41,6 +45,7 @@ export const JOBS = [
     type: "Freelance",
     date: "2023",
     description: RapsodieDesc,
+    short: "Developed new functionalities, refactoring code, ensuring seamless integration.",
     website: "https://rapsodie.co/"
   },
   {
@@ -50,6 +55,7 @@ export const JOBS = [
     type: "Freelance",
     date: "2021 - 2023",
     description: BlockEarnerDesc,
+    short: "Created cryptocurrency web application and led frontend team.",
     website: "https://blockearner.com.au/"
   },
   {
@@ -58,6 +64,7 @@ export const JOBS = [
     location: "France",
     date: "2019 - 2021",
     description: SoundchartsDesc,
+    short: "Maintenance, refactoring, and developing new features at the web app.",
     website: "https://soundcharts.com/"
   },
   {
@@ -66,6 +73,12 @@ export const JOBS = [
     location: "France",
     date: "2013 - 2018",
     description: AdomikDesc,
+    short: "Started as an intern, led frontend app development using React/Redux.",
     website: "https://www.adomik.com/"
   },
 ]
+
+export const PROFILE = {
+  short: "I‘m a fullstack engineer with over 10 years of experience, specializing in React.js, Next.js, Node.js, and Typescript with a deep expertise in frontend development.",
+  description: ProfileDesc
+}
